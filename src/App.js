837817e82d9b6eef2599ebcff212aa6b6b040362
ismail <ismailelmaslouhy@gmail.com>
2020-03-12 22:13:16 +0100

    simple exemple of useReducer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+import "./App.css";
 
-function App() {
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        counter: state.counter + action.payload
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        counter: state.counter - action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+const App = () => {
+  const initialState = {
+    counter: 0,
+    message: "Hello Devs !! this is a simple exemple of UseReducer Hooks"
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div className="wrapper">
+        <div> {state.message}</div>
+        <button
+          className="btn"
+          onClick={() => {
+            dispatch({ type: INCREMENT, payload: 10 });
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          INCREMENT
+        </button>
+        <button
+          className="btn"
+          onClick={() => {
+            dispatch({ type: DECREMENT, payload: 10 });
+          }}
+        >
+          DECREMENT
+        </button>
+        <div> {state.counter}</div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
